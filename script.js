8f9f6250c2d77b36d14b9995592820ec292a7e9d
Misbah Ansari <150408924+MisbahAnsar@users.noreply.github.com>
2024-08-26 09:38:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// script.js
-let mode = 'degree'; // Default mode
+let mode = 'degree';
 
 function appendToDisplay(value) {
     document.getElementById('display').value += value;
@@ -11,7 +10,7 @@ function clearDisplay() {
 
 function deleteLastCharacter() {
     const display = document.getElementById('display');
-    display.value = display.value.slice(0, -1); // Remove the last character
+    display.value = display.value.slice(0, -1); 
 }
 
 function calculateResult() {
@@ -19,14 +18,12 @@ function calculateResult() {
     try {
         let expression = display.value;
 
-        // Convert degrees to radians if in degree mode
         if (mode === 'degree') {
             expression = expression.replace(/Math.sin\(([^)]+)\)/g, (match, p1) => `Math.sin(${p1} * Math.PI / 180)`);
             expression = expression.replace(/Math.cos\(([^)]+)\)/g, (match, p1) => `Math.cos(${p1} * Math.PI / 180)`);
             expression = expression.replace(/Math.tan\(([^)]+)\)/g, (match, p1) => `Math.tan(${p1} * Math.PI / 180)`);
         }
 
-        // Evaluate the expression
         display.value = eval(expression);
     } catch (error) {
         display.value = 'Error';
